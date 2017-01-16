1db27b2fa2a5fff43668a3cd147f84657db69b35
BAESE <morendo90@gmail.com>
2017-01-16 11:19:02 +0900

    타이틀 추가

--- src/ExGtiBrach.java
+++ src/ExGtiBrach.java
@@ -1,6 +1,7 @@
 
 class ExGitBrach{
 	public static void main(String[] args){
+		System.out.println("git 소개");
 		System.out.println("일상적인 업무");
 		System.out.println("깃이란 무엇인가");
 		System.out.println("버전에 대한 설명");
